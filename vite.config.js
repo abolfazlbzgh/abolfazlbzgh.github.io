7d89e017ddba7a49fd7f6a311435b4915f710b34
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-05-22 09:58:50 -0500

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePages from 'vite-plugin-react-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // ... other options
-        assetFileNames: (assetInfo) => (assetInfo.name.endsWith('.jsx') ? '[name].js' : '[name].[ext]'),
-        // Add MIME type for JSX files
-        chunkFileNames: '[name].[format].js',
-        sourcemapFileNames: '[file].map',
-        // ... other options
-      },
-    },
-  },
+  base: '/',
+  plugins: [react(), vitePages()],
+ 
 })
