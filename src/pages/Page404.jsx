dcd64b513ce77aaa4197e29bbdbfc997ddc2a70b
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-05-22 23:53:49 -0500

    edit 404 page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageBox from '../components/PageBox/PageBox'
 import Information from '../components/Infromation/Information'
+import { Link } from 'react-router-dom'
 
 export default function notFound() {
     return (
@@ -10,9 +11,11 @@ export default function notFound() {
                 <section >
                     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                         <div class="mx-auto max-w-screen-sm text-center">
-                            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary dark:text-primary">404</h1>
-                            <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
-                            <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                            <h1 class="mb-4 text-5xl tracking-tight font-extrabold lg:text-7xl text">404</h1>
+                            <p class="mb-4 text-xl tracking-tight font-bold  md:text-2xl text ">Something's missing.</p>
+                            <Link to='/' className='mb-4 text-lg font-light gradient px-4 py-2 rounded-full shadow-lg text-white transition-all' >
+                                Home Page
+                            </Link>
                         </div>
                     </div>
                 </section>
