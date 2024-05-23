80e44e007f360864b4c7d48bcd552d730a189174
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-05-22 21:44:31 -0500

    change footer font size

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className=' w-full flex justify-center items-center  pb-4'>
-            <h3 className='font-light text-base text '>
+            <h3 className='font-light text-sm text md:text-base  '>
             © 2024 All Rights Reserved by Abolfazl Bazghandi.
             </h3>
         </div>
