c1a0e55ccc0b17cb23bffaa55d2a64fa1ac48208
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-05-23 09:54:27 -0500

    add basename for router

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vitePages from 'vite-plugin-react-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/abolfazlbzgh.github.io/',
   plugins: [react(), vitePages()],
  
 })
