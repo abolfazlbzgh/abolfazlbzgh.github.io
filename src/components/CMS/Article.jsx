b84025b574ad944f92fcb4495e98100c27281def
Abolfazl Bazghandi <abolfazl.bazghandi94@gmail.com>
2024-06-08 11:32:55 -0500

    add Textarea

--- src/components/CMS/Article.jsx
+++ src/components/CMS/Article.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Select from 'react-select';
 import Input from './Input';
+import Textarea from './Textarea';
 import TagItems from './TagItems';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -92,7 +93,7 @@ export default function Article({ onResult }) {
         } else if (selected.label === 'img') {
             setIsShowSrc(true)
             setIsShowContent(true)
-        }else if (selected.label === 'video') {
+        } else if (selected.label === 'video') {
             setIsShowSrc(true)
         } else { // p h html
             setIsShowContent(true)
@@ -115,12 +116,13 @@ export default function Article({ onResult }) {
                     />
                     <div className='flex justify-end items-center'>
 
-                    <button type="submit" onClick={(event) => onSubmit(event)} class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-56 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit Html Tag</button>
+                        <button type="submit" onClick={(event) => onSubmit(event)} class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-56 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit Html Tag</button>
                     </div>
                 </div>
                 <div className='flex flex-col gap-4'>
+
+                    {isShowContent && <Textarea title='content' placeholder={`Enter content of ${selectedOption.label}`} state={content} setState={setContent} />}
                     <div className='grid grid-cols-2 gap-4'>
-                        {isShowContent && <Input title='content' placeholder={`Enter content of ${selectedOption.label}`} state={content} setState={setContent} />}
                         {isShowSrc && <Input title='src' placeholder={`Enter src of ${selectedOption.label}`} state={src} setState={setSrc} />}
                         {isShowLang && <Input title='lang' placeholder={`Enter lang of ${selectedOption.label}`} state={lang} setState={setLang} />}
                         {isShowLink && <Input title='link' placeholder={`Enter link of ${selectedOption.label}`} state={link} setState={setLink} />}
